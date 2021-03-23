07977d9e55e649af57b721d3b20d9c3c9e64dcda
322160 <bagherifar.kamyab@stud.th-luebeck.de>
2021-03-23 11:49:38 +0100

    done_1

--- src/main/java/currying/Examples.java
+++ src/main/java/currying/Examples.java
@@ -6,19 +6,20 @@ public class Examples {
 
   public static Function<Integer, Integer> fact1;
   static {
-    fact1 = n -> n <= 1 ? n : n * fact1.apply(n - 1);
+    fact1 = n -> n <= 1 ? 1 : n * fact1.apply(n - 1);
   }
 
-  public final Function<Integer, Integer> fact2 = n -> n <= 1 ? n : n * this.fact2.apply(n - 1);
+  public final Function<Integer, Integer> fact2 = n -> n <= 1 ? 1 : n * this.fact2.apply(n - 1);
 
   public Function<Integer, Integer> fact3;
   {
-    fact3 = n -> n <= 1 ? n : n * fact3.apply(n - 1);
+    fact3 = n -> n <= 1 ? 1 : n * fact3.apply(n - 1);
   }
+    public static final Function<Integer, Integer> fib = f -> f <= 1 ? f : Examples.fib.apply(f-1) + Examples.fib.apply(f-2);
 
   public static void main(String[] args) {
     System.out.println(fact0.apply(10));
-    System.out.println(fact1.apply(10));
+    System.out.println(fact1.apply(0));
     Examples x = new Examples();
     System.out.println(x.fact2.apply(10));
     System.out.println(x.fact3.apply(10));
--- src/main/java/currying/Function.java
+++ src/main/java/currying/Function.java
@@ -1,5 +1,37 @@
 package currying;
 
 public interface Function<T, U> {
-  U apply(T arg);
+    U apply(T arg);
+
+    static Function<Integer, Integer> compose(Function<Integer, Integer> f1,
+                                              Function<Integer, Integer> f2) {
+        return new Function<Integer, Integer>() {
+            @Override
+            public Integer apply(Integer arg) {
+                return f1.apply(f2.apply(arg));
+            }
+        };
+    }
+
+    static Function<Integer, Integer> compose1(Function<Integer, Integer> f1,
+                                              Function<Integer, Integer> f2) {
+        return arg -> f1.apply(f2.apply(arg));
+    }
+
+
+    Function<Integer, Function<Integer, Integer>> add = x -> y -> x + y;
+
+    Function<Integer, Integer> factorial = n ->{
+        return n <= 1 ? n : n * Function.factorial.apply(n - 1);
+    };
+
+    static Function<Boolean, Boolean> not(){
+        return arg -> !arg;
+    }
+
+
+
+
+
+
 }
