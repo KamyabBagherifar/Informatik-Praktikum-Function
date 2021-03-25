3c44a50c1c270c5290e6067289f76e0ea56b050d
322160 <bagherifar.kamyab@stud.th-luebeck.de>
2021-03-25 16:21:25 +0100

    done_1

--- src/main/java/currying/Operators.java
+++ src/main/java/currying/Operators.java
@@ -3,4 +3,31 @@ package currying;
 
 public class Operators {
 
+    static int max(int a ,int b){
+        return Math.max(a, b);
+    }
+
+    static boolean not(boolean a){
+        return !a;
+    }
+
+    static boolean und(boolean a , boolean b){
+        return a || b;
+    }
+
+    static boolean oder(boolean a , boolean b){
+        return a && b;
+    }
+
+    static boolean nor(boolean a , boolean b){
+        return not(oder(a,b));
+
+    }
+
+    static boolean nand(boolean a, boolean b){
+        return not(und(a,b));
+    }
+
+
+
 }
